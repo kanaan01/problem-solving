0f4b96ae8aed4d7c2ef457ee392df906a7aee4f7
Mohammad Kanaan <mkanaan0502@gmail.com>
2023-04-07 20:58:07 +0300

    Create Replace().cs

---
+++ Replace().cs
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/577bd026df78c19bca0002c0/train/csharp
+
+public class Kata
+{
+  public static string Correct(string text)
+  {
+     
+     return text.Replace('5' , 'S').Replace('0' , 'O').Replace('1' , 'I');
+  
+  }
+}
