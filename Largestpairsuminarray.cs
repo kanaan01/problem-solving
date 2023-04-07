c608e13dd94565cbc9a3c2f48e6c7f9c44d6e3fc
Mohammad Kanaan <mkanaan0502@gmail.com>
2023-04-07 21:18:12 +0300

    Update Largestpairsuminarray.cs

--- Largestpairsuminarray.cs
+++ Largestpairsuminarray.cs
@@ -7,3 +7,13 @@ public class Kata
       return numbers.OrderByDescending(x=>x).Take(2).Sum();  
    }
 }
+
+using System.Linq;
+
+public class Kata
+{
+  public static int LargestPairSum(int[] numbers)
+  {
+    return numbers.OrderBy(i => i).TakeLast(2).Sum();
+  }
+}
