a6cdd47d5021b387e85ffe8de461f02d9d91f934
Mohammad Kanaan <mkanaan0502@gmail.com>
2023-04-09 23:04:16 +0300

    Update CountingDuplicates.cs

--- CountingDuplicates.cs
+++ CountingDuplicates.cs
@@ -23,3 +23,13 @@ public class Kata
      return repeated;
   }
 }
+
+using System.Linq;
+
+public class Kata
+{
+  public static int DuplicateCount(string str)
+  {
+    return str.ToLower().GroupBy(c => c).Where(g => g.Count() > 1).Count();
+  }
+}
